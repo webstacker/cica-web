8a5f7c48a2fbbb6962e27f219bf45c7adca9ab8b
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2019-12-06 14:52:09 +0000

    WIP

--- src/modules/autocomplete/autocomplete.js
+++ src/modules/autocomplete/autocomplete.js
@@ -40,12 +40,73 @@ function createAutocomplete() {
         }));
     }
 
+    /**
+     * Initialises the `Enhanced auto-complete` select element implementation.
+     *
+     * @param {(string|NodeList)} elements - A CSS selector for the elements, or a collection of the elements that are to be turned in to autocomplete select elements.
+     *
+     * @example
+     *
+     *     autocomplete.init('.govuk-select');
+     *     autocomplete.init(document.querySelectorAll('.govuk-select'));
+     */
+    function init(elements) {
+        let selectElements = elements;
+
+        if (typeof selectElements === 'string') {
+            selectElements = document.querySelectorAll(selectElements);
+        }
+
+        if (selectElements.length) {
+            for (let i = 0; i < selectElements.length; i += 1) {
+                selectElements[i].parentNode.classList.add('autocomplete__wrapper');
+
+                accessibleAutocomplete.enhanceSelectElement({
+                    selectElement: selectElements[i],
+                    id: 'enhanced-dropdown-id',
+                    minLength: 2,
+                    defaultValue: '',
+                    autoselect: true,
+                    confirmOnBlur: true,
+                    showAllValues: true,
+                    displayMenu: 'overlay',
+                    onConfirm,
+                    // eslint-disable-next-line no-loop-func
+                    source: (query, syncResults) => {
+                        const resultsArray = htmlCollectionToArray(selectElements[i]);
+                        const results = formatResults(resultsArray);
+
+                        syncResults(
+                            query
+                                ? results.filter(result => {
+                                      // Make the user unable to search for and select the place holder.
+                                      if (result.code === '') {
+                                          return false;
+                                      }
+                                      return (
+                                          result.name.toLowerCase().indexOf(query.toLowerCase()) !==
+                                          -1
+                                      );
+                                  })
+                                : []
+                        );
+                    },
+                    templates: {
+                        inputValue: inputValueTemplate,
+                        suggestion: suggestionTemplate
+                    }
+                });
+            }
+        }
+    }
+
     return Object.freeze({
         inputValueTemplate,
         suggestionTemplate,
         onConfirm,
         htmlCollectionToArray,
-        formatResults
+        formatResults,
+        init
     });
 }
 
