8703bb030af596e108a6c7ee3813c21f14357419
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2020-03-09 10:01:12 +0000

    WIP: blah

--- app.js
+++ app.js
@@ -71,7 +71,6 @@ app.use(
         httpOnly: true
     })
 );
-
 app.use(
     clientSessions({
         cookieName: 'cicaSession', // cookie name dictates the key name added to the request object
@@ -123,14 +122,14 @@ app.use(
     '/assets',
     express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk/assets'))
 );
-app.use(
-    '/govuk-frontend/all.css',
-    express.static(path.join(__dirname, '/public/stylesheets/all.css'))
-);
-app.use(
-    '/govuk-frontend/all-ie8.css',
-    express.static(path.join(__dirname, '/public/stylesheets/all-ie8.css'))
-);
+// app.use(
+//     '/govuk-frontend/all.css',
+//     express.static(path.join(__dirname, '/public/stylesheets/all.css'))
+// );
+// app.use(
+//     '/govuk-frontend/all-ie8.css',
+//     express.static(path.join(__dirname, '/public/stylesheets/all-ie8.css'))
+// );
 app.use(
     '/govuk-frontend/all.js',
     express.static(path.join(__dirname, '/node_modules/govuk-frontend/govuk/all.js'))
