63438888cb91bccb92f95c5341f092e9cb3258fe
Stephen <webstacker@outlook.com>
2020-03-20 00:02:22 +0000

    WIP: fancy jest command

--- jest.config.js
+++ jest.config.js
@@ -9,6 +9,15 @@ const config = {
 
 config.setupFilesAfterEnv = ['./jest.setup.js'];
 
+config.coverageThreshold = {
+    global: {
+        branches: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100
+    }
+};
+
 /*! m0-start */
 module.exports = config;
 /*! m0-end */
