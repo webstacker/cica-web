4109d62639fc41b21236d599d11aa77dab2e0f8e
Stephen <webstacker@outlook.com>
2020-03-20 00:17:11 +0000

    WIP: fancy ci command

--- jest.config.jsdom.js
+++ jest.config.jsdom.js
@@ -4,4 +4,13 @@ const config = {
     testRegex: '.*\\.test\\.jsdom\\.js$'
 };
 
+config.coverageThreshold = {
+    global: {
+        branches: 50,
+        functions: 50,
+        lines: 50,
+        statements: 50
+    }
+};
+
 module.exports = config;
